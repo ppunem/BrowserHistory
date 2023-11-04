3c108abb797082699e9554ebddc21d015a003da2
ppunem <prathyusha2094@gmail.com>
2023-11-04 13:35:16 +0000

    items not getting deleted

---
+++ src/components/HistoryItem/index.js
@@ -0,0 +1,31 @@
+import './index.css'
+
+const HistoryItem = props => {
+  const {details} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl, delItem} = details
+
+  const removeItem = () => {
+    delItem(id)
+  }
+
+  return (
+    <li className="item-container">
+      <p className="time">{timeAccessed}</p>
+      <div className="app-container">
+        <img src={logoUrl} className="app" alt="domain logo" />
+        <p className="title">{title}</p>
+        <p className="url">{domainUrl}</p>
+      </div>
+      <button className="del-button" type="button" onClick={removeItem}>
+        <img
+          testid="delete"
+          src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+          alt="delete"
+          className="delete"
+        />
+      </button>
+    </li>
+  )
+}
+
+export default HistoryItem
